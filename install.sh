85404de3d297ce9170ee8254f0fde68571c80f18
Xtempl <167797831+XXtempll@users.noreply.github.com>
2026-05-08 21:57:39 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,16 +6,15 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-# Определяем реального пользователя и его домашнюю директорию
-REAL_USER=${SUDO_USER:-$USER}
-USER_HOME=$(getent passwd "$REAL_USER" | cut -d: -f6)
-
-echo "--- 1. Репозитории и обновление системы ---"
-xbps-install -Sy void-repo-nonfree void-repo-multilib
+echo "--- 1. Подключение репозиториев и обновление ---"
+xbps-install -Sy void-repo-nonfree
 xbps-install -Syu
 
-echo "--- 2. Ядро и драйверы Wi-Fi (Broadcom) ---"
+echo "--- 2. Установка драйверов Wi-Fi (Broadcom) ---"
+# Установка заголовков ядра и DKMS (нужно для сборки драйвера wl)
 xbps-install -y dkms linux-headers broadcom-wl-dkms
+
+# Блокировка стандартных драйверов, которые ломают Wi-Fi на Lenovo
 cat <<EOF > /etc/modprobe.d/broadcom-wl.conf
 blacklist b43
 blacklist b43legacy
@@ -24,61 +23,27 @@ blacklist bcma
 blacklist brcmsmac
 EOF
 
-echo "--- 3. Сеть и DNS 8.8.8.8 ---"
+echo "--- 3. Установка NetworkManager ---"
 xbps-install -y NetworkManager
+
+echo "--- 4. Настройка DNS 8.8.8.8 (Google) ---"
+# Удаляем старый конфиг и пишем свой
 rm -f /etc/resolv.conf
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+# Делаем файл неизменяемым, чтобы DNS не "слетал"
 chattr +i /etc/resolv.conf
 
-echo "--- 4. Графический стек, звук и права (Fixing X Server) ---"
-# Добавлены все пакеты для авторизации и работы Intel HD
-xbps-install -y xorg-minimal xinit xauth xorg-server xf86-video-intel \
-mesa-dri mesa-vulkan-intel libgcc-32bit libstdc++-32bit \
-libdrm-32bit MesaLib-32bit pipewire alsa-utils-config rtkit dbus elogind polkit
-
-# КРИТИЧНО: Установка SUID бита, чтобы Xorg мог запускаться от пользователя
-chmod u+s /usr/libexec/Xorg
-
-echo "--- 5. Группы пользователя (Fixing Permissions) ---"
-for group in video input tty audio wheel storage network; do
-    groupadd -f $group
-    usermod -aG $group $REAL_USER
-done
-
-echo "--- 6. Сборка vxwm (GitHub в домашнюю директорию) ---"
-xbps-install -y base-devel libX11-devel libXft-devel libXinerama-devel git dmenu xterm feh
-
-cd "$USER_HOME"
-rm -rf vxwm
-git clone https://github.com/wh1tepearll/vxwm.git
-chown -R "$REAL_USER":"$REAL_USER" vxwm
-
-cd vxwm
-make && make install
-cd ..
-
-echo "--- 7. ПО для Web-разработки и Игры ---"
-xbps-install -y vscode-bin nodejs-lts python3 steam
-
-echo "--- 8. Включение сервисов (Runit) ---"
+echo "--- 5. Включение сервисов ---"
+# Включаем dbus и NetworkManager через систему runit
 ln -sf /etc/sv/dbus /var/service/
-ln -sf /etc/sv/elogind /var/service/
 ln -sf /etc/sv/NetworkManager /var/service/
+# Отключаем dhcpcd, чтобы он не конфликтовал с NetworkManager
 rm -f /var/service/dhcpcd
-
-echo "--- 9. Настройка .xinitrc (Fixing "Server terminated" error) ---"
-# Создаем файл .xinitrc и даем ему права, чтобы startx знал, что запускать
-cat <<EOF > "$USER_HOME/.xinitrc"
-#!/bin/sh
-exec vxwm
-EOF
-
-chown "$REAL_USER":"$REAL_USER" "$USER_HOME/.xinitrc"
-chmod +x "$USER_HOME/.xinitrc"
 
 echo "----------------------------------------------------"
 echo "УСТАНОВКА ЗАВЕРШЕНА!"
-echo "Теперь ОБЯЗАТЕЛЬНО выполни: reboot"
-echo "После перезагрузки вводи: startx"
+echo "1. Введите 'reboot' для перезагрузки."
+echo "2. После входа введите 'nmtui' для поиска Wi-Fi сетей."
+echo "3. Если сетей нет, введите: sudo modprobe wl"
 echo "----------------------------------------------------"
