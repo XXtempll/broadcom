8f21eecc7f1f78b7c1d0efd6e1c9222590409699
Xtempl <167797831+XXtempll@users.noreply.github.com>
2026-05-08 21:25:30 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,43 +1,81 @@
 #!/bin/bash
 
+# Проверка на root
 if [ "$EUID" -ne 0 ]; then 
-  echo "Запусти через sudo!"
+  echo "Ошибка: Запусти скрипт через sudo (sudo ./install.sh)"
   exit 1
 fi
 
-echo "--- 1. Исправление прав и групп ---"
+# Определяем реального пользователя и его домашнюю директорию
 REAL_USER=${SUDO_USER:-$USER}
-# Добавляем тебя во все нужные группы
-for group in video input tty wheel audio storage; do
+USER_HOME=$(getent passwd "$REAL_USER" | cut -d: -f6)
+
+echo "--- 1. Репозитории и обновление системы ---"
+xbps-install -Sy void-repo-nonfree void-repo-multilib
+xbps-install -Syu
+
+echo "--- 2. Ядро и драйверы Wi-Fi (Broadcom) ---"
+xbps-install -y dkms linux-headers broadcom-wl-dkms
+cat <<EOF > /etc/modprobe.d/broadcom-wl.conf
+blacklist b43
+blacklist b43legacy
+blacklist ssb
+blacklist bcma
+blacklist brcmsmac
+EOF
+
+echo "--- 3. Сеть и DNS 8.8.8.8 ---"
+xbps-install -y NetworkManager
+rm -f /etc/resolv.conf
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+chattr +i /etc/resolv.conf
+
+echo "--- 4. Графический стек, звук и права ---"
+xbps-install -y xorg-minimal xinit xauth xorg-server xf86-video-intel \
+mesa-dri mesa-vulkan-intel libgcc-32bit libstdc++-32bit \
+libdrm-32bit MesaLib-32bit pipewire alsa-utils-config rtkit dbus elogind polkit
+
+# Установка SUID бита на Xorg (решает проблему запуска сервера)
+chmod u+s /usr/libexec/Xorg
+
+echo "--- 5. Группы пользователя ---"
+for group in video input tty audio wheel storage network; do
     groupadd -f $group
     usermod -aG $group $REAL_USER
 done
 
-echo "--- 2. Установка критических компонентов X11 ---"
-# Добавляем xf86-video-fbdev как запасной драйвер
-xbps-install -Sy xorg-minimal xinit xauth xorg-server xf86-video-intel \
-xf86-video-fbdev mesa-dri mesa-vulkan-intel dbus elogind polkit
+echo "--- 6. Сборка vxwm (Клонирование в домашнюю директорию) ---"
+xbps-install -y base-devel libX11-devel libXft-devel libXinerama-devel git dmenu xterm feh
 
-echo "--- 3. Настройка SUID для Xorg ---"
-# Это решает проблему "Server X problem" в 90% случаев
-chmod u+s /usr/libexec/Xorg
+# Переходим в домашнюю папку пользователя
+cd "$USER_HOME"
+# Удаляем старую папку, если она была, и качаем заново
+rm -rf vxwm
+git clone https://codeberg.org/wh1tepearl/vxwm.git
+chown -R "$REAL_USER":"$REAL_USER" vxwm
 
-echo "--- 4. Пересборка vxwm (на случай ошибок библиотек) ---"
-xbps-install -y base-devel libX11-devel libXft-devel libXinerama-devel git
-rm -rf /tmp/vxwm
-git clone https://codeberg.org/wh1tepearl/vxwm.git /tmp/vxwm
-cd /tmp/vxwm && make && make install
-cd -
+# Собираем и устанавливаем
+cd vxwm
+make && make install
+cd ..
 
-echo "--- 5. Включение сервисов (КРИТИЧНО) ---"
-# Без работающего dbus и elogind X-сервер может падать
+echo "--- 7. Инструменты разработки и игры ---"
+xbps-install -y vscode-bin nodejs-lts python3 steam
+
+echo "--- 8. Включение сервисов ---"
 ln -sf /etc/sv/dbus /var/service/
 ln -sf /etc/sv/elogind /var/service/
 ln -sf /etc/sv/NetworkManager /var/service/
+rm -f /var/service/dhcpcd
 
-echo "--- 6. Настройка .xinitrc ---"
-USER_HOME=$(getent passwd "$REAL_USER" | cut -d: -f6)
+echo "--- 9. Настройка .xinitrc ---"
 echo "exec vxwm" > "$USER_HOME/.xinitrc"
 chown "$REAL_USER":"$REAL_USER" "$USER_HOME/.xinitrc"
 
-echo "--- ГОТОВО! ПЕРЕЗАГРУЗИСЬ ОБЯЗАТЕЛЬНО (reboot) ---"
+echo "----------------------------------------------------"
+echo "УСТАНОВКА ЗАВЕРШЕНА!"
+echo "Папка vxwm находится в: $USER_HOME/vxwm"
+echo "1. reboot"
+echo "2. startx"
+echo "----------------------------------------------------"
