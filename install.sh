181ee7729cc1296b2a51948bbc3671ae42755aff
Xtempl <167797831+XXtempll@users.noreply.github.com>
2026-05-08 20:57:14 +0600

    Refactor install script for clarity and updates

--- install.sh
+++ install.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# Проверка на root
+# Проверка прав
 if [ "$EUID" -ne 0 ]; then 
-  echo "Запусти скрипт через sudo: sudo ./install.sh"
+  echo "Запусти от root (sudo ./install.sh)"
   exit
 fi
 
-echo "--- 1. Настройка репозиториев и обновление ---"
+echo "--- 1. Репозитории и Обновление ---"
 xbps-install -Sy void-repo-nonfree void-repo-multilib
 xbps-install -Syu
 
-echo "--- 2. Установка ядра и драйверов Wi-Fi (Lenovo G510) ---"
-# linux-headers нужны для сборки драйвера Broadcom
+echo "--- 2. Драйверы Wi-Fi (Broadcom) и Ядро ---"
+# linux-headers необходимы для сборки модуля wl через dkms
 xbps-install -y dkms linux-headers broadcom-wl-dkms
-
-# Блокируем конфликтующие драйверы Wi-Fi
-cat <<EOF > /etc/modprobe.d/broadcom-wl.conf
+# Блокируем свободные драйверы, которые мешают работе Broadcom
+cat <<EOF> /etc/modprobe.d/broadcom-wl.conf
 blacklist b43
 blacklist b43legacy
 blacklist ssb
@@ -23,50 +22,52 @@ blacklist bcma
 blacklist brcmsmac
 EOF
 
-echo "--- 3. Установка NetworkManager и настройка DNS (8.8.8.8) ---"
+echo "--- 3. Сеть, NetworkManager и DNS 8.8.8.8 ---"
 xbps-install -y NetworkManager
-# Отключаем стандартный dhcpcd, чтобы не конфликтовал с NetworkManager
-touch /etc/resolv.conf
+# Настройка статического DNS
+rm -f /etc/resolv.conf
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 echo "nameserver 8.8.4.4" >> /etc/resolv.conf
-# Делаем файл неизменяемым, чтобы система его не переписала (защита DNS)
+# Защита файла от перезаписи (атрибут immutable)
 chattr +i /etc/resolv.conf
 
-echo "--- 4. Графика и Звук (Intel HD 4600) ---"
+echo "--- 4. Графический стек (Intel HD 4600) ---"
 xbps-install -y xorg-minimal xinit xterm mesa-dri mesa-vulkan-intel \
+libgcc-32bit libstdc++-32bit libdrm-32bit MesaLib-32bit \
 pipewire alsa-utils-config rtkit dbus elogind polkit
 
-echo "--- 5. Установка vxwm ---"
-xbps-install -y base-devel libX11-devel libXft-devel libXinerama-devel git dmenu feh
-git clone https://codeberg.org/wh1tepearl/vxwm.git
+echo "--- 5. Сборка vxwm с Codeberg ---"
+xbps-install -y base-devel libX11-devel libXft-devel libXinerama-devel feh dmenu
+rm -rf /tmp/vxwm
+git clone https://codeberg.org/wh1tepearl/vxwm.git /tmp/vxwm
 cd /tmp/vxwm && make && make install
 cd -
 
-echo "--- 6. ПО для разработки (Web) ---"
-xbps-install -y vscode-bin nodejs-lts python3 python3-pip git curl
+echo "--- 6. Инструменты разработки (Web) ---"
+xbps-install -y vscode-bin nodejs-lts git curl
 
-echo "--- 7. Гейминг (Steam + Dota 2) ---"
-# Ставим Steam и 32-битные библиотеки для него
-xbps-install -y steam libgcc-32bit libstdc++-32bit libdrm-32bit MesaLib-32bit
+echo "--- 7. Steam и Dota 2 ---"
+xbps-install -y steam
 
-echo "--- 8. Включение сервисов ---"
-# В Void сервисы включаются через создание симлинков
+echo "--- 8. Включение сервисов (runit) ---"
+# В Void мы создаем симлинки в /var/service
 ln -sf /etc/sv/dbus /var/service/
 ln -sf /etc/sv/NetworkManager /var/service/
 ln -sf /etc/sv/elogind /var/service/
-# Отключаем dhcpcd, если он был включен
+# Удаляем dhcpcd, чтобы не было конфликта с NetworkManager
 rm -f /var/service/dhcpcd
 
-echo "--- 9. Финальные штрихи ---"
-# Настройка запуска графики для пользователя
-USER_NAME=$( logname )
-USER_HOME=$(eval echo "~$USER_NAME")
-echo "exec vxwm" > "$USER_HOME/.xinitrc"
-chown $USER_NAME:$USER_NAME "$USER_HOME/.xinitrc"
+echo "--- 9. Настройка автозапуска ---"
+# Находим имя реального пользователя, который запустил sudo
+REAL_USER=${SUDO_USER:-$USER}
+USER_HOME=$(getent passwd "$REAL_USER" | cut -d: -f6)
 
-echo "----------------------------------------------------------"
-echo "УСТАНОВКА ЗАВЕРШЕНА!"
+echo "exec vxwm" > "$USER_HOME/.xinitrc"
+chown "$REAL_USER":"$REAL_USER" "$USER_HOME/.xinitrc"
+
+echo "----------------------------------------------------"
+echo "ГОТОВО! План действий:"
 echo "1. Перезагрузись: reboot"
-echo "2. После перезагрузки введи: nmtui (чтобы подключить Wi-Fi)"
-echo "3. Введи: startx (чтобы запустить vxwm)"
-echo "----------------------------------------------------------"
+echo "2. После входа введи: nmtui (настрой Wi-Fi)"
+echo "3. Введи: startx (запустится vxwm)"
+echo "----------------------------------------------------"
